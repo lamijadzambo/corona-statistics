1193dec68c055d85877331c18837539c98de380f
AlmirMBH <almir.mustafic.tz@gmail.com>
2021-01-27 11:49:17 +0100

    excel controller and upload blade

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExcelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/upload', [ExcelController::class, 'show'])->name('upload-excel');
+Route::post('/upload/store', [ExcelController::class, 'store'])->name('store-excel');
+
