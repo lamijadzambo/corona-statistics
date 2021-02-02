8bb6260ff6496789f2755df2e07ab240f715e593
SamerSacic <samer.sacic@gmail.com>
2021-02-02 17:31:02 +0100

    List population with DataTables library

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -5,12 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Population;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Yajra\DataTables\DataTables;
 
 class DataController extends Controller
 {
     public function index(){
-        $year = 2020;
         $population = Population::all();
         $deaths = DB::table('death_rate_sums')->get(); //This is a SUM of columns of deaths table fetched as db view
         $deathRate = [];
@@ -21,21 +19,4 @@ class DataController extends Controller
 
         return view('index', compact('population', 'deathRate'));
     }
-
-
-    public function getStudents(Request $request)
-    {
-        if ($request->ajax()) {
-            $populationData = Population::latest()->get();
-            return Datatables::of($populationData)
-                ->addIndexColumn()
-                ->addColumn('action', function($row){
-                    $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
-                    return $actionBtn;
-                })
-                ->rawColumns(['action'])
-                ->make(true);
-        }
-    }
-
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,14 +16,20 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
+    <!-- Datatable Style -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.23/r-2.2.7/datatables.min.css"/>
+
+    <!-- Bootstrap -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Main -->
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+            <div class="container-fluid px-5">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
@@ -77,8 +83,19 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div class="container-fluid">
+                @yield('content')
+            </div>
         </main>
     </div>
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <!-- Datatables -->
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.23/r-2.2.7/datatables.min.js" defer></script>
+
+    @yield('script')
 </body>
 </html>
